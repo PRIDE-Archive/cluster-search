b123bcb94b186198623b404dfee43b900e5b8915
noedelta <noedelta@gmail.com>
2015-04-18 21:05:58 +0100

    Adds the new search methods

--- src/main/java/uk/ac/ebi/pride/cluster/search/service/IClusterSearchService.java
+++ src/main/java/uk/ac/ebi/pride/cluster/search/service/IClusterSearchService.java
@@ -22,21 +22,34 @@ public interface IClusterSearchService {
     Page<SolrCluster> findByHighestRatioPepSequences(Set<String> sequences, Pageable pageable);
 
     /**
-     * Return filtered clusters (or not) by modifications names with the highlights for sequences
-     * @param sequences (peptide sequences to search) mandatory
-     * @param modNameFilters (modification names to search) optional
+     * Return filtered clusters (or not) by modifications names and species names and peptides with the highlights for sequences
+     * @param query (term to search by in "text" and in "highest ratio peptide sequences" fields ) mandatory
+     * @param sequenceFilters (peptide sequences to filter by) optional
+     * @param modNameFilters (modification names to filter by) optional
+     * @param speciesNameFilters (species names to filter by) optional
      * @param pageable requested page
      * @return A page with the clusters and the highlights snippets
      */
-    PageWrapper<SolrCluster> findByHighestRatioPepSequencesHighlightsOnModificationNames(Set<String> sequences, Set<String> modNameFilters, Pageable pageable);
+    PageWrapper<SolrCluster> findByTextAndHighestRatioPepSequencesHighlightsFilterOnModificationNamesAndSpeciesNames(
+            String query,
+            Set<String> sequenceFilters,
+            Set<String> modNameFilters,
+            Set<String> speciesNameFilters,
+            Pageable pageable);
 
     /**
-     * Count the facets per modification name
-     * @param sequences (peptide sequences to search) mandatory
-     * @param modNameFilters (modification names to search) optional
-     * @return a map with the mod_names and the number of hits per mod_synonym
+     * Count the facets per modification name and species names
+     * @param query (term to search by in text field and highest ratio peptide sequences) mandatory
+     * @param sequenceFilters (peptide sequences to filter by) optional
+     * @param modNameFilters (modification names to filter by) optional
+     * @param speciesNameFilters (species names to filter by) optional
+     * @return a map per facet field with the names and the number of hits per name
      */
-    Map<String, Long> findByHighestRatioPepSequencesFacetOnModificationNames(Set<String> sequences, Set<String> modNameFilters);
+    Map<String, Map<String, Long>> findByTextAndHighestRatioPepSequencesFacetOnModificationNamesAndSpeciesNames(
+            String query,
+            Set<String> sequenceFilters,
+            Set<String> modNameFilters,
+            Set<String> speciesNameFilters);
 
     Page<SolrCluster> findByNearestPeaks(String quality, double precursorMz, double windowSize, double[] mzValues, double[] intensityValues, Pageable pageable);
 
